dbb5a7bf293000c6b7e4ed66004018261fec6e88
Rohit <rohithrajka524@gmail.com>
2025-10-03 17:52:55 +0530

    fix: oauth callback issue fixed

--- server/config/passport.js
+++ server/config/passport.js
@@ -24,7 +24,7 @@ passport.use(new JwtStrategy({
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: `${process.env.SERVER_URL}api/auth/google/callback`
+  callbackURL: `${process.env.SERVER_URL}/api/auth/google/callback`
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     // Check if user already exists
